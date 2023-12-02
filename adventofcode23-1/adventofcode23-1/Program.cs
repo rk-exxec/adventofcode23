720510864d6798961a11a66bbce69e225e2cf8c5
rk-exxec <40037381+rk-exxec@users.noreply.github.com>
2023-12-02 18:32:45 +0100

    day 1-1 and day 2 full

---
+++ adventofcode23-1/adventofcode23-1/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Advent of code 2023 - Day 1");
+
+var input = System.IO.File.ReadLines("S:\\git_repos\\adventofcode23\\adventofcode23-1\\input.txt");
+
+int sum = 0;
+foreach (var line in input)
+{
+    if (string.IsNullOrEmpty(line)) continue;
+    int firstDigit = -1, lastDigit = -1;
+
+    foreach (char symbol in line)
+    {
+        if ('0' <= symbol && symbol <= '9')
+        {
+            var number = symbol - '0';
+            if (firstDigit == -1) firstDigit = number;
+            lastDigit = number; 
+        }
+        
+    }
+    sum += firstDigit * 10 + lastDigit;
+}
+
+Console.WriteLine($"Output Part 1: {sum}");
+
+//------------------------------------------
+
+input = System.IO.File.ReadLines("S:\\git_repos\\adventofcode23\\adventofcode23-1\\input.txt");
+//input = @"two1nine
+//eightwothree
+//abcone2threexyz
+//xtwone3four
+//4nineeightseven2
+//zoneight234
+//7pqrstsixteen
+//".Split('\n');
+
+sum = 0;
+foreach (var line in input)
+{
+    if (string.IsNullOrEmpty(line)) continue;
+    int firstDigit = -1, lastDigit = -1;
+
+    var numberWordMatches = Regex.Matches(line, "(?:one|two|three|four|five|six|seven|eight|nine|[0-9])").ToList();
+    var numberWordDict = new Dictionary<string, int>() { 
+        { "one", 1 },   { "two", 2 },   { "three", 3 }, { "four", 4 },  { "five", 5 },  { "six", 6 },   { "seven", 7 }, { "eight", 8 }, { "nine", 9 },
+        {"1",1 },       {"2",2 },       {"3",3 },       {"4",4 },       {"5",5 },       {"6",6 },       {"7",7 },       {"8",8 },       {"9",9 } 
+    };
+
+
+    firstDigit = numberWordDict[numberWordMatches.First().Value];
+    lastDigit = numberWordDict[numberWordMatches.Last().Value];
+    var value = firstDigit * 10 + lastDigit;
+    //Console.WriteLine(value );
+    sum += value;
+}
+
+Console.WriteLine($"Output Part 2: {sum}");
---
+++ adventofcode23-2/adventofcode23-2/Program.cs
@@ -0,0 +1,79 @@
+﻿
+Console.WriteLine("Advent of code 2023 - Day 2");
+
+var input = System.IO.File.ReadLines("S:\\git_repos\\adventofcode23\\adventofcode23-2\\input.txt");
+
+int sum = 0;
+var invalidGames = new List<int>();
+foreach (var line in input)
+{
+    var gameSplit = line.Split(":");
+    int gameID = int.Parse(gameSplit[0].Split(" ")[1]);
+    var setsSplit = gameSplit[1].Split(";");
+    var gameValid = true;
+
+    foreach (var set in setsSplit)
+    {
+        var cubesSplit = set.Split(",");
+        var numRedCubes = 0;
+        var numGreenCubes = 0;
+        var numBlueCubes = 0;
+        foreach (var cubes in cubesSplit) 
+        {
+            var amount = int.Parse(cubes.Trim().Split(" ")[0]);
+            var color = cubes.Trim().Split(" ")[1];
+            switch (color )
+            {
+                case "red": numRedCubes = amount; break;
+                case "green": numGreenCubes = amount; break;
+                case "blue": numBlueCubes = amount; break;
+            }
+        }
+        if (numRedCubes > 12 || numGreenCubes > 13 || numBlueCubes > 14)
+        {
+            gameValid = false;
+            break;
+        }
+    }
+    if (gameValid) sum += gameID;
+    else invalidGames.Add(gameID);
+}
+
+Console.WriteLine($"Output Part 1: {sum}");
+//invalidGames.ForEach(Console.WriteLine);
+
+// -----------------------------------------
+input = System.IO.File.ReadLines("S:\\git_repos\\adventofcode23\\adventofcode23-2\\input.txt");
+
+sum = 0;
+foreach (var line in input)
+{
+    var gameSplit = line.Split(":");
+    int gameID = int.Parse(gameSplit[0].Split(" ")[1]);
+    var setsSplit = gameSplit[1].Split(";");
+
+    var numRedCubes = 0;
+    var numGreenCubes = 0;
+    var numBlueCubes = 0;
+
+    foreach (var set in setsSplit)
+    {
+        var cubesSplit = set.Split(",");
+        foreach (var cubes in cubesSplit)
+        {
+            var amount = int.Parse(cubes.Trim().Split(" ")[0]);
+            var color = cubes.Trim().Split(" ")[1];
+            // only keep max amount
+            switch (color)
+            {
+                case "red": numRedCubes = (amount > numRedCubes) ? amount : numRedCubes; break;
+                case "green": numGreenCubes = (amount > numGreenCubes) ? amount : numGreenCubes; break;
+                case "blue": numBlueCubes = (amount > numBlueCubes) ? amount : numBlueCubes; break;
+            }
+        }
+    
+    }
+    sum += numRedCubes*numGreenCubes*numBlueCubes;
+}
+
+Console.WriteLine($"Output Part 2: {sum}");
